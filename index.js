9019ba602a0afb87c243a037959b7f8cd6c7fe3a
soumik825 <allhafesh825@gmail.com>
2022-11-24 21:12:44 +0600

    install jws and save user in database

--- index.js
+++ index.js
@@ -2,12 +2,44 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 7000;
 const app = express();
-
-
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s3uhktq.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+
+        const usersCollection = client.db('poridhan').collection('users');
+
+
+
+        // user create add in db
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
 
 
 app.get('/', async (req, res) => {
